80a4bc66908881892da243ac9fe6b47b1c476df5
hannahrolleman <hannroll516@ed.amdsb.ca>
2017-09-21 12:38:53 -0400

    Add project files.

---
+++ PacMan/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace PacMan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Startbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Startbutton
+            // 
+            this.Startbutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
+            this.Startbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Startbutton.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Startbutton.ForeColor = System.Drawing.Color.White;
+            this.Startbutton.Location = new System.Drawing.Point(74, 79);
+            this.Startbutton.Name = "Startbutton";
+            this.Startbutton.Size = new System.Drawing.Size(131, 77);
+            this.Startbutton.TabIndex = 0;
+            this.Startbutton.Text = "Begin Game";
+            this.Startbutton.UseVisualStyleBackColor = true;
+            this.Startbutton.Click += new System.EventHandler(this.Startbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Startbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Startbutton;
+    }
+}
+
---
+++ PacMan/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PacMan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Startbutton_Click(object sender, EventArgs e)
+        {
+            Startbutton.Visible = false; //makes button disapear
+             
+            Graphics formGraphics = this.CreateGraphics();
+            Graphics fg = this.CreateGraphics();
+
+            Pen drawPen = new Pen(Color.Blue, 10);
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0,70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250,20, 250, 300);
+
+            drawPen.Color = Color.Yellow;
+            SolidBrush drawBrush = new SolidBrush(Color.Yellow);
+            formGraphics.FillPie(drawBrush, 10, 25, 35, 35, 30, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 50, 25, 35, 35, 30, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 90, 25, 35, 35, 30, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 130, 25, 35, 35, 30, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 170, 25, 35, 35, 30, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 210, 25, 35, 35, 30, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 210, 25, 35, 35, 120, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 210, 50, 35, 35, 120, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 210, 75, 35, 35, 120, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 210, 100, 35, 35, 120, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 210, 125, 35, 35, 120, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 210, 150, 35, 35, 120, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 210, 175, 35, 35, 120, 300);
+
+            Thread.Sleep(300);
+            formGraphics.Clear(Color.Black);
+
+            drawPen.Color = Color.Blue;
+            formGraphics.DrawLine(drawPen, 0, 20, 250, 20);
+            formGraphics.DrawLine(drawPen, 0, 70, 200, 70);
+            formGraphics.DrawLine(drawPen, 200, 70, 200, 300);
+            formGraphics.DrawLine(drawPen, 250, 20, 250, 300);
+
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillPie(drawBrush, 210, 200, 35, 35, 120, 300);
+
+            drawBrush.Color = Color.White; 
+            Font drawFont = new Font("Arial", 12, FontStyle.Bold);
+            fg.DrawString("Well done!", drawFont, drawBrush, 50, 200);
+        }
+    }
+}
